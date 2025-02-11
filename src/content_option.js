0accf4e9cae898b02aa62c784063afa38af9f176
HARSH SHARMA <harsh.sharma.8986@gmail.com>
2025-02-11 19:19:31 +0530

    err

---
+++ src/content_option.js
@@ -0,0 +1,100 @@
+const logotext = "HARSH";
+const meta = {
+    title: "Harsh Sharma",
+    description: "CS Undergrad | Competitive Programmer | Backend Developer",
+};
+
+const introdata = {
+    title: "Hi, I'm Harsh Sharma",
+    animated: {
+        first: "I love problem solving",
+        second: "I do Competitive Programmer",
+        third: "I build Backend",
+    },
+    description: "I'm a third year CS undergrad with a deep interest in backend development and problem solving.",
+    your_img_url: "https://res.cloudinary.com/dpwqggym0/image/upload/v1739276720/5_ryt8i6.jpg",
+};
+
+const dataabout = {
+    title: "About Me",
+    aboutme: "I'm a CS undergrad with a deep interest in competitive programming, backend development, and system design. With experience in MERN stack and a strong grasp of algorithms, I love solving challenging problems and building efficient applications.",
+};
+
+const worktimeline = [
+    {
+        jobtitle: "GDGC DSA/CP Lead",
+        where: "Google's Developer Club",
+        date: "Oct 2024 - Present",
+    },
+    {
+        jobtitle: "Core Techinical Team",
+        where: "BBDITM Technical Club",
+        date: "Dec 2024 - Present",
+    },
+    {
+        jobtitle: "Open Source Contributor (Hacktober Fest)",
+        where: "GitHub",
+        date: "Oct 2024",
+    },
+    
+];
+
+const skills = [
+    { name: "C++ ", value: 85 },
+    { name: "Data Structures & Algorithms", value: 80 },
+    { name: "JavaScript (ES6+)", value: 70 },
+    { name: "ReactJs", value: 70 },
+    { name: "Node.js & Express", value: 80 },
+    { name: "MongoDB", value: 75 },
+    { name: "System Design & OS Concepts", value: 50 },
+];
+
+const dataportfolio = [
+    {
+        img: "https://res.cloudinary.com/dpwqggym0/image/upload/v1739279251/blog_a9vvrt.jpg",
+        description: "A blog website build with React and Node JS.",
+        link: "https://github.com/harsSharma45/Sumrise",
+    },
+    {
+        img: "https://res.cloudinary.com/dpwqggym0/image/upload/v1739279251/blog_a9vvrt.jpg",
+        description: "A real-time chat application with WebSockets, built using MERN stack.",
+        link: "#",
+    },
+    {
+        img: "https://res.cloudinary.com/dpwqggym0/image/upload/v1739279251/blog_a9vvrt.jpg",
+        description: "An Accountibility App for productivity.",
+        link: "#",
+    },
+    {
+        img: "https://res.cloudinary.com/dpwqggym0/image/upload/v1739279251/blog_a9vvrt.jpg",
+        description: "TnP cell webiste.",
+        link: "#",
+    },
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "imharsharma45@gmail.com",
+    YOUR_FONE: "+91 XXX-XXX-XXXX",
+    description: "Always open to collaboration, projects, and exciting opportunities in tech.",
+    YOUR_SERVICE_ID: "service_s228jio", 
+    YOUR_TEMPLATE_ID: "template_qfic2ka",  
+    YOUR_USER_ID: "wLRCGREmGFhI_AtvG",  
+};
+
+const socialprofils = {
+    github: "https://github.com/harsSharma45",
+    linkedin: "https://linkedin.com/in/harsharma45",
+    instagram: "https://twitter.com/visitorx7",
+};
+
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    worktimeline,
+    skills,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
